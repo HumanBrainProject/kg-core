fdd90a913913f09f0f97ddb0fc8c51ec1a5f187b
Oliver Schmid <oliver.schmid@epfl.ch>
2020-06-26 09:13:03 +0200

    declare bundle properties for proper overriding

--- bundle/kg-core-allinone/src/main/java/eu/ebrains/kg/KgCoreAllInOne.java
+++ bundle/kg-core-allinone/src/main/java/eu/ebrains/kg/KgCoreAllInOne.java
@@ -19,10 +19,12 @@ package eu.ebrains.kg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @Configuration
+@PropertySource("classpath:bundle.properties")
 @EnableAsync
 public class KgCoreAllInOne {
 
