a0795123a57c3fa4f0bc05d214c4fec1291a9194
Oliver Schmid <oliver.schmid@epfl.ch>
2021-05-12 15:25:15 +0200

    fix import issue

--- libs/kg-microservice-commons/src/main/java/eu/ebrains/kg/commons/RequestLoggingFilter.java
+++ libs/kg-microservice-commons/src/main/java/eu/ebrains/kg/commons/RequestLoggingFilter.java
@@ -35,6 +35,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 import java.util.UUID;
 
 @Component
